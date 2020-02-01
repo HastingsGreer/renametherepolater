151aa0291a908a5c39ada14083f89e0cf62b6f0b
Austin Wang <austin.wang0311@gmail.com>
2020-01-31 22:08:11 -0500

    Non-functional stuff

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+////// Here, we initialize the pixi application
+var pixiRoot = new PIXI.Application(800, 600, { backgroundColor : 0x6BACDE });
+
+// add the renderer view element to the DOM
+document.body.appendChild(pixiRoot.view);
+
+////// Here, we create our traviso instance and add on top of pixi
+
+// engine-instance configuration object
+var instanceConfig = {
+    mapDataPath: "/static/map/mapData.json", // the path to the json file that defines map data, required
+    assetsToLoad: ["t1.png", "box.png"] // array of paths to the assets that are desired to be loaded by traviso, no need to use if assets are already loaded to PIXI cache, default null
+};
+
+var engine = TRAVISO.getEngineInstance(instanceConfig);
+pixiRoot.stage.addChild(engine);
