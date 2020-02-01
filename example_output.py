98da86b345d1fcb904a1a2197d795aa136e74ef2
Yicheng Wang <alex.wyc2098@protonmail.ch>
2020-01-31 21:14:51 -0500

    i thought js was more powerful than that but okay...

--- example_output.py
+++ example_output.py
@@ -1,25 +1,41 @@
-example_json = {
-        "board":[[
-            {
-            "environment": 
-                {"background":"grass", "improvements":[]},
-            "unit": 0
-            } for i in range(5)] for j in range(5)],
-        "animations": [
-            {
-               "type": "tree_rocket",
-               "start": [1, 3],
-               "end": [4, 2]
-            },
-        ]
-}
-
-example_json["board"][1][3]["unit"] = {"type": "treebuchet", "happiness":3, "owner":0}
-
-example_json["board"][4][4]["environment"]["improvements"].append("flowers")
-
 import json
 
-print(json.dumps(example_json))
+def example_json():
+    example_json = {
+            "board":[[
+                {
+                "environment": 
+                    {"background":"grass", "improvements":[]},
+                "unit": {}
+                } for i in range(5)] for j in range(5)],
+            "animations": [
+                {
+                   "type": "tree_rocket",
+                   "start": [1, 3],
+                   "end": [4, 2]
+                },
+            ]
+    }
 
+    example_json["board"][1][3]["unit"] = {"id": 0, "type": "treebuchet",
+            "happiness":3, "owner":0}
+    example_json["board"][3][3]["unit"] = {"id": 1, "type": "flower_girl",
+            "happiness":3, "owner":1}
+    example_json["board"][4][4]["environment"]["improvements"].append("flowers")
+
+    return example_json
+
+def example_client_reply():
+    return {
+        'moves': [
+            { 'id': 0, "start": [1, 3], "end": [1, 4] },
+            { 'id': 1, "start": [4, 4], "end": [2, 2] }
+        ],
+        'attacks': [
+            { 'id': 0, 'target': [1, 2] }
+        ]
+    }
+
+if __name__ == '__main__':
+    print(json.dumps(example_json()))
 
